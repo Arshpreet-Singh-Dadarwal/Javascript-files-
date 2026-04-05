1c825dc8e6547c174cbbe2cc721c09e885e88d29
Arshpreet Singh <arshpreetsinghdadarwal@gmail.com>
2026-04-05 18:55:02 +0000

    datatypes

--- containter-test-basic-js/datatypes-in-js.js
+++ containter-test-basic-js/datatypes-in-js.js
@@ -1,3 +1,17 @@
 "use strict"; //treat all code as newer version
 
 //alert(3+3) //we are using nodejs not brower so not work here
+let age=19
+let islogin=false
+let state=null//null datatype is object
+
+//number=>2 to power 53
+//string=""
+//boolean=true/false
+//null=>standalone value(empty)
+//undefined=>varible declared but not assign any value
+
+//symbol=>unique
+//object
+console.log(typeof state)//object
+console.log(typeof undefined)//undefined
