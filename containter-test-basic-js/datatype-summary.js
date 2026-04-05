7510180b0d7b9b724e46cf77d70a1500baec9877
Arshpreet Singh <arshpreetsinghdadarwal@gmail.com>
2026-04-05 19:42:24 +0000

    update in datatype-summary

--- containter-test-basic-js/datatype-summary.js
+++ containter-test-basic-js/datatype-summary.js
@@ -35,3 +35,21 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++]
+// stack(primitive) and Heap(non-primitive)
+
+let user1="arshpreetsingh"
+let user2=user1 //pass the copy of user1
+console.table([user1,user2])
+user2="kush"
+console.table([user1,user2])
+
+let myobj1={
+    email:"kush@gmail.com",
+    name:"kush"
+}
+let myobj2=myobj1 //pass by reference(if change in obj2 then change also occur in obj1)
+myobj2.email="arshpreet@gmail.com"
+console.log(myobj1.email)
+console.log(myobj2.email)
